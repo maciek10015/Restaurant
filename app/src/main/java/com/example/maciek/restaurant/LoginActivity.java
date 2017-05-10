849d7da91ff2dd875f762dd3ee045c7a787ffb7e
Maciek <maciek10015@gmail.com>
2017-05-10 22:03:52 +0200

    add-login-layout

---
+++ app/src/main/java/com/example/maciek/restaurant/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.maciek.restaurant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Maciek on 10.05.2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
--- app/src/main/java/com/example/maciek/restaurant/MainActivity.java
+++ app/src/main/java/com/example/maciek/restaurant/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.maciek.restaurant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "onCreate");
+        startActivityButton = (Button) findViewById(R.id.button_admin);
+        startActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
